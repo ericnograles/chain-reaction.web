527f70af750afe95d449f73aea9ce3f7d4647de4
Eric Nograles <ENograles@352inc.com>
2016-03-19 11:37:07 -0400

    mdl-js-layout CANNOT be the root of a React component

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -38,47 +38,49 @@ class Home extends Component {
     const hasLoginError = this.props.user.error;
 
     return (
-      <div className="demo-layout mdl-layout mdl-layout--fixed-header mdl-js-layout mdl-color--grey-100">
-        <header className="demo-header mdl-layout__header mdl-layout__header--scroll mdl-color--grey-100 mdl-color-text--grey-800">
-          <div className="mdl-layout__header-row">
-            <span className="mdl-layout-title">Chain Reaction</span>
-            <div className="mdl-layout-spacer"></div>
-            <div className="mdl-textfield mdl-js-textfield mdl-textfield--expandable">
-              <label className="mdl-button mdl-js-button mdl-button--icon" htmlFor="search">
-                <i className="material-icons">search</i>
-              </label>
-              <div className="mdl-textfield__expandable-holder">
-                <input className="mdl-textfield__input" type="text" id="search" />
-                <label className="mdl-textfield__label" htmlFor="search">Enter your query...</label>
+      <div>
+        <div className="demo-layout mdl-layout mdl-layout--fixed-header mdl-js-layout mdl-color--grey-100">
+          <header className="demo-header mdl-layout__header mdl-layout__header--scroll mdl-color--grey-100 mdl-color-text--grey-800">
+            <div className="mdl-layout__header-row">
+              <span className="mdl-layout-title">Chain Reaction</span>
+              <div className="mdl-layout-spacer"></div>
+              <div className="mdl-textfield mdl-js-textfield mdl-textfield--expandable">
+                <label className="mdl-button mdl-js-button mdl-button--icon" htmlFor="search">
+                  <i className="material-icons">search</i>
+                </label>
+                <div className="mdl-textfield__expandable-holder">
+                  <input className="mdl-textfield__input" type="text" id="search" />
+                  <label className="mdl-textfield__label" htmlFor="search">Enter your query...</label>
+                </div>
               </div>
             </div>
-          </div>
-        </header>
-        <div className="demo-ribbon"></div>
-        <main className="demo-main mdl-layout__content">
-          <div className="demo-container mdl-grid">
-            <div className="mdl-cell mdl-cell--2-col mdl-cell--hide-tablet mdl-cell--hide-phone"></div>
-            <div className="demo-content mdl-color--white mdl-shadow--4dp content mdl-color-text--grey-800 mdl-cell mdl-cell--8-col">
-              <LoginForm email={email}
-                         password={password}
-                         user={user}
-                         handleEmail={this.handleEmail}
-                         handlePassword={this.handlePassword}
-                         handleLogin={this.handleLogin} />
-              <div className="login-error">
-                {hasLoginError ? 'Error logging in. ' + this.props.user.error : null}
+          </header>
+          <div className="demo-ribbon"></div>
+          <main className="demo-main mdl-layout__content">
+            <div className="demo-container mdl-grid">
+              <div className="mdl-cell mdl-cell--2-col mdl-cell--hide-tablet mdl-cell--hide-phone"></div>
+              <div className="demo-content mdl-color--white mdl-shadow--4dp content mdl-color-text--grey-800 mdl-cell mdl-cell--8-col">
+                <LoginForm email={email}
+                           password={password}
+                           user={user}
+                           handleEmail={this.handleEmail}
+                           handlePassword={this.handlePassword}
+                           handleLogin={this.handleLogin} />
+                <div className="login-error">
+                  {hasLoginError ? 'Error logging in. ' + this.props.user.error : null}
+                </div>
               </div>
             </div>
-          </div>
-          <footer className="demo-footer mdl-mini-footer">
-            <div className="mdl-mini-footer--left-section">
-              <ul className="mdl-mini-footer--link-list">
-                <li><a href="https://facebook.github.io/react/docs/getting-started.html">React Documentation</a></li>
-                <li><a href="https://github.com/facebook/react">React GitHub</a></li>
-              </ul>
-            </div>
-          </footer>
-        </main>
+            <footer className="demo-footer mdl-mini-footer">
+              <div className="mdl-mini-footer--left-section">
+                <ul className="mdl-mini-footer--link-list">
+                  <li><a href="https://facebook.github.io/react/docs/getting-started.html">React Documentation</a></li>
+                  <li><a href="https://github.com/facebook/react">React GitHub</a></li>
+                </ul>
+              </div>
+            </footer>
+          </main>
+        </div>
       </div>
     );
   }
