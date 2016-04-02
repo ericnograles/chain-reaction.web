fb376af90427a8b8ec56e0875eaf811aebfba946
Eric Nograles <ENograles@352inc.com>
2016-04-02 10:13:28 -0400

    Dem template updates doe

--- src/containers/Dashboard/index.js
+++ src/containers/Dashboard/index.js
@@ -20,10 +20,26 @@ class Dashboard extends Component {
   render() {
     var { user } = this.props;
     return (
-      <div className="meme-container">
-        <div className="welcome">
-          Welcome to Chain Reaction, {user.email}
-          <input type="button" value="Log Out" onClick={this.handleLogout} />
+      <div>
+        <div className="demo-layout mdl-layout mdl-layout--fixed-header mdl-js-layout mdl-color--grey-100">
+          <header className="demo-header mdl-layout__header mdl-layout__header--scroll mdl-color--grey-100 mdl-color-text--grey-800">
+            <div className="mdl-layout__header-row">
+              <span className="mdl-layout-title">Chain Reaction</span>
+              <div className="mdl-layout-spacer"></div>
+              <div className="mdl-textfield mdl-js-textfield mdl-textfield--expandable">
+                <input type="button" value="Log Out" onClick={this.handleLogout} />
+              </div>
+            </div>
+          </header>
+          <div className="demo-ribbon"></div>
+          <main className="demo-main mdl-layout__content">
+            <div className="demo-container mdl-grid">
+              <div className="mdl-cell mdl-cell--2-col mdl-cell--hide-tablet mdl-cell--hide-phone"></div>
+              <div className="demo-content mdl-color--white mdl-shadow--4dp content mdl-color-text--grey-800 mdl-cell mdl-cell--8-col">
+                The Chains they be Reactin'
+              </div>
+            </div>
+          </main>
         </div>
       </div>
     )
