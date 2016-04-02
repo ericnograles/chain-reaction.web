60ee5af774882d72bfedae3da0e4a9ed3418f5b3
Eric Nograles <ENograles@352inc.com>
2016-04-02 10:28:32 -0400

    Prettifying the memes

--- src/components/Meme/index.js
+++ src/components/Meme/index.js
@@ -4,8 +4,11 @@ export default class Meme extends Component {
   render() {
     var { url } = this.props;
     return (
-      <div className="meme-image">
-        <img src={url} />
+      <div className="demo-container mdl-grid">
+        <div className="mdl-cell mdl-cell--2-col mdl-cell--hide-tablet mdl-cell--hide-phone"></div>
+        <div className="demo-content mdl-color--white mdl-shadow--4dp content mdl-color-text--grey-800 mdl-cell mdl-cell--8-col">
+          <img src={url} />
+        </div>
       </div>
     );
   }
