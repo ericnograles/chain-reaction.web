405f4cc59f4d986390689e04344ac2c7de6a0582
Eric Nograles <ENograles@352inc.com>
2016-04-03 06:29:52 -0400

    Renamed "reactClass" to "component".  More appropriate naming

--- src/components/Meme/template.js
+++ src/components/Meme/template.js
@@ -1,12 +1,12 @@
 import React from "react";
 
-export function MemeTemplate(reactClass) {
-  var props = reactClass.props;
+export function MemeTemplate(component) {
+  var props = component.props;
   return (
     <div className="demo-container mdl-grid">
       <div className="mdl-cell mdl-cell--2-col mdl-cell--hide-tablet mdl-cell--hide-phone"></div>
       <div className="demo-content mdl-color--white mdl-shadow--4dp content mdl-color-text--grey-800 mdl-cell mdl-cell--8-col">
-        <img src={reactClass.props.url} />
+        <img src={component.props.url} />
       </div>
     </div>
   );
